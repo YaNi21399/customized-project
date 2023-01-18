5012b33006e2b938a017f8897e486f3c6540082c
YaNi21399 <90896873+YaNi21399@users.noreply.github.com>
2023-01-19 07:57:49 +0800

    sncond change vue config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/customized-project/dist/'
+  publicPath: '/customized-project/public/'
 })
